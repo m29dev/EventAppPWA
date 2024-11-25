a676ecf4e4273a96d3b07db2005d73504e6d7d2a
m29dev <michalmajchrzak.it@gmail.com>
2024-11-25 13:33:34 +0100

    serviceWorker fix

--- src/index.js
+++ src/index.js
@@ -15,8 +15,21 @@ root.render(
     </React.StrictMode>
 )
 
-// Uncomment this line to enable service worker
-ServiceWorkerRegistration.register()
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then((registration) => {
+                console.log(
+                    'Service Worker registered with scope:',
+                    registration.scope
+                )
+            })
+            .catch((error) => {
+                console.log('Service Worker registration failed:', error)
+            })
+    })
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
